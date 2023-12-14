5b2c59086e02c51ff1b3043451e03f0b54479796
Bastian Stark <53190965+bastian-stark@users.noreply.github.com>
2023-12-14 10:29:21 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,64 @@
+import random
+
+def initialize_board(word):
+    board = ''
+    for n in range(0, len(word)):
+        board += '_'
+    return board
+
+def check_letter(word, userGuess):
+    status = 0
+    for char in word:
+        if userGuess == char:
+            status += 1
+    return status
+
+def updateBoard(board, word, letter):
+    boardList = []
+    for x in board:
+        boardList.append(x)
+    for y in range(0, len(word)):
+        if word[y] == letter:
+            boardList[y] = letter
+    board = ''
+    for z in boardList:
+        board += z
+    return board
+
+def checkStatus(board):
+    charsRevealed = 0
+    for j in range(0, len(board)):
+        if board[j] != '_':
+            charsRevealed += 1
+    if charsRevealed == len(board):
+        return 1
+    else:
+        return 0
+
+
+word_list = ["city", "tree", "computer", "phone", "dog"]
+
+print('Welcome to Hangman')
+
+word = random.choice(word_list)
+board = initialize_board(word)
+
+gameStatus = 0
+lives = 3
+while gameStatus != 1 and lives > 0:
+    print(board)
+    userGuess = input('Guess a letter: ').lower()
+    if check_letter(word, userGuess) > 0:
+        board = updateBoard(board, word, userGuess)
+        gameStatus = checkStatus(board)
+        print('Correct!')
+    else:
+        lives -= 1
+        print('Incorrect!')
+
+if gameStatus == 1 and lives > 0:
+    print(f'You win! The word was "{word}". Game over.')
+elif lives == 0:
+    print(f'You lose. The word was "{word}". Game over.')
+else:
+    print('Error')
